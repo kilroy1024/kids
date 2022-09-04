25f8b8d8afdddcaea3557f5ee72532839077da24
kilroy <kilroy1024@gmail.com>
2022-09-04 22:21:53 +0800

    add basic socket connection function

--- main.c
+++ main.c
@@ -4,6 +4,12 @@
 #include <string.h>
 #include <sys/time.h>
 #include <unistd.h> 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
 
 #define TIMES 10
 #define MULTIPLICAND_START  (3)
@@ -17,6 +23,8 @@
 enum EMENU_SET_CMD {
     EMENU_TIME_TBL,
     EMENU_TEST,
+    EMENU_SVR,
+    EMENU_CLIENT,
     EMENU_SET_CMD_NUM
 };
 
@@ -253,15 +261,121 @@ static void times_table(struct pri_data *pri_data)
 
 }
 
+static int svr(struct pri_data *pri_data)
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = { 0 };
+    char* hello = "Hello from server";
+  
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0))
+        == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+ 
+ /*
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET,
+                   SO_REUSEADDR | SO_REUSEPORT, &opt,
+                   sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+*/
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+  
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr*)&address,
+             sizeof(address))
+        < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket
+         = accept(server_fd, (struct sockaddr*)&address,
+                  (socklen_t*)&addrlen))
+        < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read(new_socket, buffer, 1024);
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+    
+  // closing the connected socket
+    close(new_socket);
+  // closing the listening socket
+    shutdown(server_fd, SHUT_RDWR);
+    return 0;
+}
+
+static int client(struct pri_data *pri_data)
+{
+    int sock = 0, valread, client_fd;
+    struct sockaddr_in serv_addr;
+    char* hello = "Hello from client";
+    char buffer[1024] = { 0 };
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+  
+    // Convert IPv4 and IPv6 addresses from text to binary
+    // form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+        <= 0) {
+        printf(
+            "\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+  
+    if ((client_fd
+         = connect(sock, (struct sockaddr*)&serv_addr,
+                   sizeof(serv_addr)))
+        < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+  
+    // closing the connected socket
+    close(client_fd);
+    return 0;
+}
+
 static void menu_set(struct pri_data *pri_data)
 {
-    printf("!! pri_data = %d\n", pri_data->cmd);
-
     switch (pri_data->cmd) {
     case EMENU_TEST:
     	printf("test %d\n", EMENU_TEST);
         break;
+    case EMENU_SVR:
+	printf("server :\n");
+	svr(pri_data);
+    	break;
+    case EMENU_CLIENT:
+	printf("client :\n");
+	client(pri_data);
+    	break;
     case EMENU_TIME_TBL:
+
     default:
         times_table(pri_data);
     }
